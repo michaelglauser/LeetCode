a9add4c572c092007918295ce6bfe0c8e57bac35
michaelglauser <glauser.michael@gmail.com>
2024-10-17 10:38:01 -0700

    Create Best_Time_to_Buy_and_Sell_Stock_II.py

---
+++ Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -0,0 +1,11 @@
+#  Best Time to Buy and Sell Stock II
+class Solution:
+  def maxProfit(self, prices: List[int]) -> int:
+    sell = 0
+    hold = -math.inf
+
+    for price in prices:
+      sell = max(sell, hold + price)
+      hold = max(hold, sell - price)
+
+    return sell

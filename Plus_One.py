81588345d1213f4e4958ab8d68c0000a24ae8ba5
michaelglauser <glauser.michael@gmail.com>
2024-10-17 10:51:10 -0700

    Create Plus_One.py

---
+++ Plus_One.py
@@ -0,0 +1,10 @@
+#Plus One, Array
+class Solution:
+  def plusOne(self, digits: List[int]) -> List[int]:
+    for i, d in reversed(list(enumerate(digits))):
+      if d < 9:
+        digits[i] += 1
+        return digits
+      digits[i] = 0
+
+    return [1] + digits
